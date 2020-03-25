f39b35efba8268a6b1592d7db269303da58b4683
Lewis Cooper <lewiscooper2392@gmail.com>
2020-03-24 17:32:06 -0700

    Updated test program auto_garden.py

--- Software/auto_garden.py
+++ Software/auto_garden.py
@@ -1,8 +1,10 @@
-import serial
+import serial, time
+from datetime import datetime
+from pathlib import Path
 
 class Controller:
     def __init__(self, port, baudrate):
-        self.ser = serial.Serial(port, baudrate, timeout=1)
+        self.ser = serial.Serial(port, baudrate)
         
     def __del__(self):
         self.ser.close()
@@ -23,19 +25,37 @@ class Controller:
         return self._read()
         
     def toggle_motor(self, index):
-        if (index >= 0 and index < 4):
-            cmd = str(index + 3)
-            self._write(cmd)
+        if (index >= 0 and index <= 3):
+            if index == 0:
+                self._write("3")
+            elif index == 1:
+                self._write("4")
+            elif index == 2:
+                self._write("5")
+            elif index == 3:
+                self._write("6")
             return self._read()
         else:
             return "Error. Invalid motor index."
-        
+
+    def stop_motors(self):
+        self._write("7")
+        return self._read()
+
 ctrl =  Controller('/dev/ttyS0', 9600)
 f = open("data.csv", "a")
-        msg += datum + ','
+
+data = ctrl.get_humidity()
+data = data.decode('ascii')
+data = data.strip('\n')
+data = data.split(' ')
+msg = ""
+for i in range(0, len(data)):
+	datum = str(int(data[i]))
+	msg += datum + ','
+
 msg += datetime.now().strftime("%m/%d/%Y,%H:%M:%S") 
 msg += "\n"
 print(msg)
 f.write(msg)
 f.close()
-
