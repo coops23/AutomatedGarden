1ca97cb9afeb02923c587ec15e96f2948a2c35a1
coops23 <lewiscooper2392@gmail.com>
2020-04-01 11:02:01 -0700

    test

--- Software/website.py
+++ Software/website.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, redirect, url_for
+from graphing import parse_data
 import psutil
 import datetime
 import os
 import Controller
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 app = Flask(__name__)
 ctrl = Controller.Controller('/dev/ttyS0', 9600)
@@ -45,6 +48,21 @@ def data_log():
     templateData = template(text = msg)
     return render_template('main.html', **templateData)
 
+@app.route("/plot.png")
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
+    
 @app.route("/motor_status")
 def motor_status():
    msg = ""
