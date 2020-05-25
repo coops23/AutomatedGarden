77244d2ca9b143ac5fcb4382382f1c5780b60a7d
Lewis Cooper <lewiscooper2392@gmai.com>
2020-05-25 15:56:09 -0700

    sampling values now uses a median value in a list of discrete samples

--- Software/sample.py
+++ Software/sample.py
@@ -1,20 +1,22 @@
 import Controller
 import datetime
+import math
+import statistics
+
+SAMPLE_COUNT = 10
 
 if __name__ == "__main__":            
     ctrl =  Controller.Controller('/dev/ttyS0', 9600)
 
-    avg = 0
+    median = 0
+    samples = []
     now = datetime.datetime.now()
     date_time = now.strftime("%m/%d/%Y %H:%M:%S")
-    for i in range(0,10):
-        try:
-            data = ctrl.get_humidity()
-            avg += data
-        except ValueError as e:
-            print(e)
-    avg = int(avg / 10)
-
+    for i in range(0, SAMPLE_COUNT):
+        samples.append(ctrl.get_humidity())
+    median = int(statistics.median(samples))
+    print(samples)
+    print(median)
     f = open("/home/pi/Desktop/AutomatedGarden/Software/data.csv", "a")
-    f.write(date_time + "," + str(avg) + "," + "False" + "\n")
+    f.write(date_time + "," + str(median) + "," + "False" + "\n")
     f.close()
--- Software/water_plants.py
+++ Software/water_plants.py
@@ -1,19 +1,22 @@
 import Controller
 import datetime
 import time
+import statistics
 
-SAMPLE_COUNT = 50
+SAMPLE_COUNT = 10
 WATERING_TIME_SECS = 20
-MOISTURE_THRESHOLD = 720
+MOISTURE_THRESHOLD = 750
 
 if __name__ == "__main__":            
     ctrl =  Controller.Controller('/dev/ttyS0', 9600)
 
     moisture = 0
+    data = []
     for i in range(0, SAMPLE_COUNT):
-        data = ctrl.get_humidity()
-        moisture += data
-    moisture = int(moisture / SAMPLE_COUNT)
+        data.append(ctrl.get_humidity())
+    moisture = int(statistics.median(data))
+    print(data)
+    print(moisture)
 
     now = datetime.datetime.now()
     date_time = now.strftime("%m/%d/%Y %H:%M:%S")
